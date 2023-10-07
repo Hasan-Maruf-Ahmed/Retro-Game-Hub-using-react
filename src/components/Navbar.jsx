2ffb78c7c5f1a48e50bf85ca3115148008379fcd
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2023-10-07 13:42:20 +0600

    Created a dropdown menu.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,70 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+import logout from "../assets/log-out.png"
+import user from "../assets/user.png"
+import settings from "../assets/settings.png"
+import edit from "../assets/edit.png"
+import envelop from "../assets/envelope.png"
+import help from "../assets/question.png"
 
 export const Navbar = () => {
+  const [open, setOpen]= useState(false);
+
+  let menuRef = useRef();
+
+  useEffect(() =>{
+    let handler = (e)=>{
+      if(!menuRef.current.contains(e.target)){
+      setOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handler);
+     return() =>{
+      document.removeEventListener("mousedown", handler);
+     }
+  })
+
   return (
     <nav>
-      <ul>
+      <ul className="nav-element">
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink className="navLink" to="/">Home</NavLink>
         </li>
         <li>
-          <NavLink to="/games">Games</NavLink>
+          <NavLink className="navLink" to="/games">Games</NavLink>
         </li>
         <li>
-          <NavLink to="/about">About</NavLink>
+          <NavLink className="navLink" to="/about">About</NavLink>
         </li>
         <li>
-          <NavLink to="/contact">Contact Us</NavLink>
+          <NavLink className="navLink" to="/contact">Contact Us</NavLink>
         </li>
       </ul>
-      <NavLink to="/profile">Profile</NavLink>
+      <div className="menu-container" ref={menuRef}>
+        <div className="menu-trigger" onClick={()=>{setOpen(!open)}}>
+          <i className='bx bx-sm bxs-user bx-border-circle'></i>
+        </div>
+        <div className={`dropdown-menu ${open? 'active' : 'inactive'}`}>
+          <ul>
+          <DropdownItem img={user} text={"My Profile"} />
+          <DropdownItem img={edit} text={"Edit profile"} />
+          <DropdownItem img={envelop} text={"Inbox"} />
+          <DropdownItem img={settings} text={"Settings"} />
+          <DropdownItem img={help} text={"Helps"} />
+            <DropdownItem img={logout} text={"Log out"} />
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
+
+function DropdownItem (props){
+  return (
+    <li className="dropdownItem">
+      <img src={props.img}  ></img>
+      <a href="">{props.text}</a>
+    </li>
+  )
+}
--- src/components/pages/tetrisGame/TetrisPage.jsx
+++ src/components/pages/tetrisGame/TetrisPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import "./styles/tetrispage.css"
 import Game from './gameComponents/Game'
 
